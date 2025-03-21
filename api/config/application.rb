0241819783ddaff9a70e5c0b6857e4452d949918
Dominik Zoricic <dominik.zoricic10000@gmail.com>
2025-03-22 00:00:41 +0100

    config fix

--- api/config/application.rb
+++ api/config/application.rb
@@ -41,6 +41,6 @@ module Api
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.action_controller.default_protect_from_forgery = true
+    config.action_controller.default_protect_from_forgery = false
   end
 end
